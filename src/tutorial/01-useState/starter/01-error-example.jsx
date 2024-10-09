c6c1607d362f35366380cd53574d3f0edab98e2a
Artem Trifonov <artemis.levitt@gmail.com>
2024-10-09 11:23:31 +0200

    Solution for first challenge

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,21 @@
+import {useState} from "react";
+
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  }
+
+  return <div>
+    <h2>{count}</h2>
+    <button
+        className='btn'
+        type="button"
+        onClick={handleClick}>Count++</button>
+  </div>;
 };
+
 
 export default ErrorExample;
