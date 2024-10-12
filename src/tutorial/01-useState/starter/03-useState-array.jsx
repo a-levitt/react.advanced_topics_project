4f08359d4ade1542fec0d272cbcbab16634f98b6
Artem Trifonov <artemis.levitt@gmail.com>
2024-10-12 13:34:46 +0200

    Final solution for task 3

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -3,12 +3,39 @@ import {data} from "../../../data.js"
 
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
+
+  const removeItem = (id) => {
+    const newPeople = people.filter((person)=>
+    person.id !== id)
+    setPeople(newPeople);
+  }
+
+  const clearAllItems = () => {
+    setPeople([]);
+  }
+
   return <div>
     {people.map((person) => {
       const {id, name} = person;
-      return <div key={id}>{name}</div>;
+      return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button
+                type='button'
+                onClick={() => removeItem(id)}>
+              remove
+            </button>
+          </div>
+      );
     })}
-  </div>;
+    <button
+        className='btn'
+        type='button'
+        style={{ marginTop: '2rem' }}
+        onClick={clearAllItems}>
+      clear all
+    </button>
+  </div>
 };
 
 export default UseStateArray;
